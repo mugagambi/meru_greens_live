1acadccbe0f7904734cb25c22e3bc1eaba2d2032
Humphrey Mugambi <mugagambi@gmail.com>
2018-01-20 11:03:05 +0300

    compiled assets for production

--- laravel/webpack.mix.js
+++ laravel/webpack.mix.js
@@ -12,53 +12,53 @@ let mix = require('laravel-mix');
  */
 
 mix.styles([
-    'public/css/bootstrap.min.css',
-    'public/css/font-awesome.css',
-    'public/css/custom-fonts.css',
-    'public/css/theme.css',
-    'public/css/animate.css',
-    'public/css/style.css',
-    'public/skins/green.css',
-    'public/css/bg10.css'
-], 'public/css/styles.css');
+    '../public_html/css/bootstrap.min.css',
+    '../public_html/css/font-awesome.css',
+    '../public_html/css/custom-fonts.css',
+    '../public_html/css/theme.css',
+    '../public_html/css/animate.css',
+    '../public_html/css/style.css',
+    '../public_html/skins/green.css',
+    '../public_html/css/bg10.css'
+], '../public_html/css/styles.css');
 mix.styles([
-    'public/admin/css/bootstrap.css',
-    'public/admin/css/font-awesome.css',
-    'public/admin/css/ionicons.css',
-    'public/admin/css/AdminLTE.css',
-    'public/admin/css/skin-green.css'
-], 'public/admin/css/admin.css');
+    '../public_html/admin/css/bootstrap.css',
+    '../public_html/admin/css/font-awesome.css',
+    '../public_html/admin/css/ionicons.css',
+    '../public_html/admin/css/AdminLTE.css',
+    '../public_html/admin/css/skin-green.css'
+], '../public_html/admin/css/admin.css');
 mix.styles([
-    'public/admin/css/bootstrap.css',
-    'public/admin/css/font-awesome.css',
-    'public/admin/css/ionicons.css',
-    'public/admin/css/AdminLTE.css'
-], 'public/admin/css/admin_login.css');
+    '../public_html/admin/css/bootstrap.css',
+    '../public_html/admin/css/font-awesome.css',
+    '../public_html/admin/css/ionicons.css',
+    '../public_html/admin/css/AdminLTE.css'
+], '../public_html/admin/css/admin_login.css');
 mix.styles([
-    'public/css/cart.css'
-], 'public/css/cart.min.css');
+    '../public_html/css/cart.css'
+], '../public_html/css/cart.min.css');
 mix.scripts([
-    'public/js/jquery.min.js',
-    'public/js/modernizr.custom.js',
-    'public/js/jquery.easing.1.3.js',
-    'public/js/bootstrap.min.js',
-    'public/js/jquery.flexslider-min.js',
-    'public/js/flexslider.config.js',
-    'public/js/jquery.appear.js',
-    'public/js/js/classie.js',
-    'public/js/uisearch.js',
-    'public/js/animate.js',
-    'public/js/custom.js',
-    'public/js/contactform.js'
-], 'public/js/scripts.js');
+    '../public_html/js/jquery.min.js',
+    '../public_html/js/modernizr.custom.js',
+    '../public_html/js/jquery.easing.1.3.js',
+    '../public_html/js/bootstrap.min.js',
+    '../public_html/js/jquery.flexslider-min.js',
+    '../public_html/js/flexslider.config.js',
+    '../public_html/js/jquery.appear.js',
+    '../public_html/js/js/classie.js',
+    '../public_html/js/uisearch.js',
+    '../public_html/js/animate.js',
+    '../public_html/js/custom.js',
+    '../public_html/js/contactform.js'
+], '../public_html/js/scripts.js');
 mix.scripts([
-    'public/admin/js/jquery.js',
-    'public/admin/js/bootstrap.js',
-    'public/admin/js/fastclick.js',
-    'public/admin/js/adminlte.js',
-    'public/admin/js/jquery.slimscroll.js'
-], 'public/admin/js/admin.js');
+    '../public_html/admin/js/jquery.js',
+    '../public_html/admin/js/bootstrap.js',
+    '../public_html/admin/js/fastclick.js',
+    '../public_html/admin/js/adminlte.js',
+    '../public_html/admin/js/jquery.slimscroll.js'
+], '../public_html/admin/js/admin.js');
 mix.scripts([
-    'public/admin/js/jquery.js',
-    'public/admin/js/bootstrap.js'
-], 'public/admin/js/admin_login.js');
+    '../public_html/admin/js/jquery.js',
+    '../public_html/admin/js/bootstrap.js'
+], '../public_html/admin/js/admin_login.js');
